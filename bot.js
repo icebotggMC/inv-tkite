79a762f2d6dd3d86f25f82eb09f4734f60b3f004
icebotggMC <41833445+icebotggMC@users.noreply.github.com>
2018-12-22 23:45:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -73,7 +73,7 @@ if (message.content.toLowerCase().startsWith(prefix + `new`)) {
         message.channel.send(`:white_check_mark: Your ticket has been created, #${c.name}.`);
         const embed = new Discord.RichEmbed()
         .setColor(0xCF40FA)
-        .addField(`Hey ${message.author.username}!`, `Please try explain why you opened this ticket with as much detail as possible. Our **Support Team** will be here soon to help.`)
+        .addField(`انت ${message.author.username}!`, `يرجى توضح لما تم فتح هاذي التذكره لنسططيع مساعدتك بما تحتاج و نقدم لك الدعم الازم`)
         .setTimestamp();
         c.send({ embed: embed });
     }).catch(console.error);
